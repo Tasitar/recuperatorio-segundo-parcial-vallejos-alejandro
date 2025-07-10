acdae0c1e8e7235d95038d342a53bbb2b4b7e380
alejandroiv <alejandroiv177771@gmail.com>
2025-07-10 15:51:43 -0300

    cree la conexion a la BD

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import sequelize from "./src/config/database.js";
+
+dotenv.config();
 
 const app = express()
 
 app.listen(process.env.PORT, ()=>
     console.log("hola soy el server y estoy prendido Bv")
 )
+
+const connectDB = async () =>{
+    try{
+        await sequelize.authenticate();
+        console.log('hola soy la base de datos y estoy conectada ;D')
+
+        await sequelize.sync({alter: true});
+    }
+}
