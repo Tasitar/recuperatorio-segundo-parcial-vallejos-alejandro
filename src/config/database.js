fd73b385e47fa7c8476a672c2d016bb52186206a
alejandroiv <alejandroiv177771@gmail.com>
2025-07-10 16:13:21 -0300

    arregle una mala exportacion en database

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config()
 
-export const connectDB = new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -13,3 +13,4 @@ export const connectDB = new Sequelize(
     }
 )
 
+export default sequelize
